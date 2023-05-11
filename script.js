90094711ffcf7e01dced6cd096184239a2e82b86
karendaysuwolfe <116389700+karendaysuwolfe@users.noreply.github.com>
2023-05-11 12:13:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,34 @@
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+let inventory = {
+  laptop: 5,
+  projector: 2,
+  microphone: 10
+};
+
+function checkIn() {
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let date = document.getElementById('date').value;
+  let equipment = document.getElementById('equipment').value;
+  
+  // Update inventory count
+  inventory[equipment] += 1;
+  
+  alert(`${name} has checked in a ${equipment}.`);
 }
 
-body {
-  font-family: Arial, sans-serif;
-  background-color: #f4f4f4;
-}
-
-.container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-h1 {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-label {
-  font-weight: bold;
-  margin-bottom: 10px;
-}
-
-input {
-  padding: 10px;
-  margin-bottom: 20px;
-  border-radius: 5px;
-  border: none;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-}
-
-.buttons {
-  display: flex;
-  justify-content: center;
-}
-
-button {
-  padding: 10px 20px;
-  margin: 0 10px;
-  border-radius: 5px;
-  border: none;
-  background-color: #4caf50;
-  color: #fff;
-  font-weight: bold;
-  cursor: pointer;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-}
-
-button:hover {
-  background-color: #3e8e41;
+function checkOut() {
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let date = document.getElementById('date').value;
+  let equipment = document.getElementById('equipment').value;
+  
+  // Check if there is equipment left in inventory
+  if (inventory[equipment] > 0) {
+    // Update inventory count
+    inventory[equipment] -= 1;
+    
+    alert(`${name} has checked out a ${equipment}.`);
+  } else {
+    alert(`Sorry, there is no ${equipment} left in inventory.`);
+  }
 }
